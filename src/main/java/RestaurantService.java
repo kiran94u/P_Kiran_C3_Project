e1076d4a04ef1090fb738f75c2f2f1f0fb4a2fb9
kiran94u <ramkiran435@yahoo.com>
2022-09-11 23:32:38 +0530

    Part 3: Failing Test case

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -30,4 +30,7 @@ public class RestaurantService {
     public List<Restaurant> getRestaurants() {
         return restaurants;
     }
+    public  int calculateOrder(String restaurantName,List<String> orderedItems){
+        return 0;
+    }
 }
--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -1,5 +1,10 @@
 import org.junit.jupiter.api.*;
+
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -9,19 +14,21 @@ class RestaurantServiceTest {
     Restaurant restaurant;
 
     @BeforeAll
-    public void setup() {
+    public void setup()
+    {
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
-        restaurant = service.addRestaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
-        restaurant = service.addRestaurant("The Lapino's Restaurant", "Chennai", openingTime, closingTime);
-        restaurant.addToMenu("Sweet corn soup", 119);
+        Restaurant restaurantToRemove = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant = service.addRestaurant("The Lapino's Restaurant","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
         restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Pasta",300);
+        restaurant.addToMenu("Coffee",150);
     }
-
     @Test
     public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
-        Restaurant restaurant1 = service.findRestaurantByName("The Lapino's Restaurant");
-        assertEquals(restaurant, restaurant1);
+        Restaurant restaurant1=service.findRestaurantByName("The Lapino's Restaurant");
+        assertEquals(restaurant,restaurant1);
         //WRITE UNIT TEST CASE HERE
     }
 
@@ -29,7 +36,7 @@ class RestaurantServiceTest {
     @Test
     public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
         //WRITE UNIT TEST CASE HERE
-        assertThrows(restaurantNotFoundException.class, () -> service.findRestaurantByName("Pantry d'or"));
+        assertThrows(restaurantNotFoundException.class,()->service.findRestaurantByName("Pantry d'or"));
     }
     //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
 
@@ -38,18 +45,30 @@ class RestaurantServiceTest {
     public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
         int initialNumberOfRestaurants = service.getRestaurants().size();
         service.removeRestaurant("Amelie's cafe");
-        assertEquals(initialNumberOfRestaurants - 1, service.getRestaurants().size());
+        assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
     }
 
     @Test
     public void removing_restaurant_that_does_not_exist_should_throw_exception() {
-        assertThrows(restaurantNotFoundException.class, () -> service.removeRestaurant("Pantry d'or"));
+        assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
     }
 
     @Test
-    public void add_restaurant_should_increase_list_of_restaurants_size_by_1() {
+    public void add_restaurant_should_increase_list_of_restaurants_size_by_1()
+    {
         int initialNumberOfRestaurants = service.getRestaurants().size();
-        service.addRestaurant("Pumpkin Tales", "Chennai", LocalTime.parse("12:00:00"), LocalTime.parse("23:00:00"));
-        assertEquals(initialNumberOfRestaurants + 1, service.getRestaurants().size());
+        service.addRestaurant("Pumpkin Tales","Chennai",LocalTime.parse("12:00:00"),LocalTime.parse("23:00:00"));
+        assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
     }
+    //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    //<<<<<<<<<<<<<<<<<<<<CALCULATE ORDER>>>>>>>>>>>>>>>>>>>>>>>>>>//
+    @Test
+    public void adding_items_should_calculate_correct_order_value()
+    {
+        List<String> itemsToOrder= new ArrayList<>(Arrays.asList("Coffe","Vegetable lasagne"));
+        int orderValue=service.calculateOrder(restaurant.getName(),itemsToOrder);
+        assertEquals(orderValue,419);
+    }
+    //<<<<<<<<<<<<<<<<<<<<CALCULATE ORDER>>>>>>>>>>>>>>>>>>>>>>>>>>//
 }
